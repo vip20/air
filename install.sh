545b377c16e7611212d8262d5e0ef3e41725d7ce
Vinay <pvinayvinu20@gmail.com>
2022-08-12 14:13:53 +0530

    log version

--- install.sh
+++ install.sh
@@ -288,6 +288,7 @@ github_release() {
   json=$(http_copy "$giturl" "Accept:application/json")
   test -z "$json" && return 1
   version=$(echo "$json" | tr -s '\n' ' ' | sed 's/.*"tag_name":"//' | sed 's/".*//')
+  log_crit "version ===>> ${version}"
   test -z "$version" && return 1
   echo "$version"
 }
